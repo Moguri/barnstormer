c93d74bc770a4757c834de8400232ce26ed353f8
Mitchell Stokes <mogurijin@gmail.com>
2013-06-13 15:13:47 -0700

    Adding yaw control to the player controller. The movement is now also setup as a matrix transform.

--- src/scripts/player_controller.py
+++ src/scripts/player_controller.py
@@ -1,7 +1,11 @@
 from bge import logic, events
-from mathutils import Vector
+from mathutils import Vector, Matrix
+
+import math
 
 class PlayerController:
+	DYAW = math.radians(1.0)
+
 	def __init__(self, ob):
 		self.obj = ob
 		print("Attaching player controller to", ob)
@@ -9,14 +13,26 @@ class PlayerController:
 	def run(self):
 		# Default values
 		throttle = 1
+		yaw = 0
 
 		# Get input
 		for keycode, status in logic.keyboard.active_events.items():
 			if keycode == events.SPACEKEY:
 				throttle = 2
+			elif keycode == events.LEFTARROWKEY:
+				yaw -= self.DYAW
+			elif keycode == events.RIGHTARROWKEY:
+				yaw += self.DYAW
 
-		# Move forward (down the -y)
-		self.obj.localPosition += Vector((0, -throttle, 0))
+		transform = self.obj.localTransform
+
+		# Move forward (down the local -y)
+		transform = Matrix.Translation((0, -throttle, 0)) * transform
+
+		# Adjust yaw (local z)
+		transform = Matrix.Rotation(yaw, 4, 'Z') * transform
+
+		self.obj.localTransform = transform
 
 def main(cont):
 	ob = cont.owner
