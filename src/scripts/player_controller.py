42bb80e5e772d50af69be7da10920b3d5af9df61
Mitchell Stokes <mogurijin@gmail.com>
2013-06-13 14:39:53 -0700

    Adding some basic throttle control. The plane should now also move forward.

--- src/scripts/player_controller.py
+++ src/scripts/player_controller.py
@@ -1,4 +1,5 @@
-from bge import logic
+from bge import logic, events
+from mathutils import Vector
 
 class PlayerController:
 	def __init__(self, ob):
@@ -6,7 +7,16 @@ class PlayerController:
 		print("Attaching player controller to", ob)
 
 	def run(self):
-		pass
+		# Default values
+		throttle = 1
+
+		# Get input
+		for keycode, status in logic.keyboard.active_events.items():
+			if keycode == events.SPACEKEY:
+				throttle = 2
+
+		# Move forward (down the -y)
+		self.obj.localPosition += Vector((0, -throttle, 0))
 
 def main(cont):
 	ob = cont.owner
