e9f901dbf7439c914db24f43c6615794989f4d3a
Mitchell Stokes <mogurijin@gmail.com>
2013-06-13 19:33:06 -0700

    Adding a scaling factor to our player controller calculations so our behavior is no longer bound to the current tick rate.

--- src/scripts/player_controller.py
+++ src/scripts/player_controller.py
@@ -2,15 +2,18 @@ from bge import logic, events
 from mathutils import Vector, Matrix
 
 import math
+import time
 
 class PlayerController:
 	DYAW = math.radians(1.0)
 	DPITCH = math.radians(1.0)
 	IDLE_THROTTLE = 0.25
 	MAX_THROTTLE = 0.35
+	TICK_RATE = 1/60  # Expected tic rate
 
 	def __init__(self, ob):
 		self.obj = ob
+		self.last_update = time.time()
 		print("Attaching player controller to", ob)
 
 	def run(self):
@@ -18,6 +21,9 @@ class PlayerController:
 		throttle = self.IDLE_THROTTLE
 		yaw = 0
 		pitch = 0
+
+		dt = time.time() - self.last_update
+		dtscale = dt / self.TICK_RATE
 
 		# Get input
 		for keycode, status in logic.keyboard.active_events.items():
@@ -33,13 +39,13 @@ class PlayerController:
 				pitch -= self.DPITCH
 
 		# Adjust pitch (local x)
-		transform = Matrix.Rotation(pitch, 4, 'X')
+		transform = Matrix.Rotation(pitch * dtscale, 4, 'X')
 
 		# Adjust yaw (local z)
-		transform = Matrix.Rotation(yaw, 4, 'Z') * transform
+		transform = Matrix.Rotation(yaw * dtscale, 4, 'Z') * transform
 
 		# Move forward (down the local -y)
-		transform = Matrix.Translation((0, -throttle, 0)) * transform
+		transform = Matrix.Translation((0, -throttle * dtscale, 0)) * transform
 
 		self.obj.localTransform = self.obj.localTransform * transform
 
@@ -47,7 +53,9 @@ class PlayerController:
 		if yaw == 0:
 			error = 0 - self.obj.localOrientation.to_euler()[1]
 			#print(error)
-			self.obj.applyRotation((0, error*0.03, 0), True)
+			self.obj.applyRotation((0, error*0.03*dtscale, 0), True)
+
+		self.last_update = time.time()
 
 def main(cont):
 	ob = cont.owner
