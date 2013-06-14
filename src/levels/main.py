90ccfd829d02852b734e24b8c5dc57846a2bdba5
Daniel Stokes <kupomail@gmail.com>
2013-06-13 20:22:55 -0700

    Adding the path hack to main.py

--- src/levels/main.py
+++ src/levels/main.py
@@ -1,12 +1,18 @@
-from bge import logic
 import sys
+import os
+
+
+from bge import logic
+
 
 def init():
-	sys.path.append(".."),
+	sys.path.append("..")
+	os.chdir(logic.expandPath("//"))
 
 def level_select(cont):
 	init()
 	logic.startGame(logic.expandPath("//free_roam.blend"))
+
 
 def free_roam(cont):
 	init()
