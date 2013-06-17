dd536c86753c0528c0a13f762abe75c582b37964
Mitchell Stokes <mogurijin@gmail.com>
2013-06-16 21:23:51 -0700

    Changing main.py and free_roam.blend so we can avoid calling main.init() and main.free_roam() twice.

--- src/levels/main.py
+++ src/levels/main.py
@@ -5,9 +5,15 @@ import os
 from bge import logic
 
 
+def update_path():
+	if ".." not in sys.path:
+		sys.path.append("..")
+		os.chdir(logic.expandPath("//"))
+
+
 def init():
-	sys.path.append("..")
-	os.chdir(logic.expandPath("//"))
+	update_path()
+
 
 def level_select(cont):
 	init()
